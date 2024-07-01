58ff5e4b3825409c29f6773783e70a17d3712e60
iconlab <icon@ufba.br>
2024-07-01 01:49:45 -0300

    initial commit (macOS)

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,64 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri_plugin_positioner::{Position, WindowExt};
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
+
+fn main() {
+    let quit = CustomMenuItem::new("quit", "Quit").accelerator("Cmd+Q");
+
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_menu(system_tray_menu).with_title("app"))
+        .setup(|app| {
+            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+
+            app.listen_global("quit", | _ | {
+                std::process::exit(0);
+            });
+
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(
+                &window,
+                NSVisualEffectMaterial::Menu,
+                Some(NSVisualEffectState::Active),
+                Some(6.0)
+            ).expect("Unsuported platform! Only MacOS is supported!");
+            
+            Ok(())
+        })
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
+                SystemTrayEvent::LeftClick { 
+                    position: _, 
+                    size: _, 
+                    .. 
+                } => {
+                    let window = app.get_window("main").unwrap();
+                    
+                    let _ = window.move_window(Position::TrayCenter);
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                },
+
+                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
